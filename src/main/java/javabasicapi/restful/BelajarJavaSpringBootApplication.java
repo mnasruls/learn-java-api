368fd4797ea19f541ab365e6e31787d2e21175bb
Muh Nasrul <muh.nasrul@youtap.id>
2025-05-15 09:10:50 +0700

    init project

---
+++ src/main/java/javabasicapi/restful/BelajarJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package javabasicapi.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javabasicapi/restful/BelajarJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package javabasicapi.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
